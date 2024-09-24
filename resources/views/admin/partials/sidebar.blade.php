83b992d308bad22746c5f5535ca5a186df75e5de
Krystian <krystian.jagiello.99@gmail.com>
2024-09-24 20:20:17 +0200

    Add route to sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -4,7 +4,7 @@
         <div class="logo text-center py-3">
             <img src="{{ asset('images/admin/svg/logo-no-background.svg') }}" alt="Logo" class="logo-sidebar">
         </div>
-<hr/>
+        <hr/>
         <ul class="nav flex-column">
             <li class="nav-item">
                 <a class="nav-link {{ Request::is('panel-admin') ? 'active' : '' }}" href="{{ route('admin.dashboard') }}">
@@ -12,22 +12,22 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('panel-admin/article') ? 'active' : '' }}" href="#">
+                <a class="nav-link {{ Request::is('panel-admin/articles') ? 'active' : '' }}" href="{{ route('admin.articles') }}">
                     <i class="fas fa-file-alt me-2"></i> <span>Artykuły</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('categories*') ? 'active' : '' }}" href="#">
+                <a class="nav-link {{ Request::is('panel-admin/categories') ? 'active' : '' }}" href="{{ route('admin.categories') }}">
                     <i class="fas fa-folder me-2"></i> <span>Kategorie</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('comments*') ? 'active' : '' }}" href="#">
+                <a class="nav-link {{ Request::is('panel-admin/comments') ? 'active' : '' }}" href="{{ route('admin.comments') }}">
                     <i class="fas fa-comments me-2"></i> <span>Komentarze</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('users*') ? 'active' : '' }}" href="#">
+                <a class="nav-link {{ Request::is('panel-admin/users') ? 'active' : '' }}" href="{{ route('admin.users') }}">
                     <i class="fas fa-users me-2"></i> <span>Użytkownicy</span>
                 </a>
             </li>
